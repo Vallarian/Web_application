9bc84cca0dbb182b9c92bb8425efd8574ee9ec27
Vallarian <danil.vorsin89@mail.ru>
2022-10-17 12:54:08 +0300

    Add new

---
+++ MyCoreWebApp/MyCoreWebApp/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCoreWebApp.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MyCoreWebApp/MyCoreWebApp/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCoreWebApp.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+    }
+}
