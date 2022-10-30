d42995cee58000638ccbdd72bc03aa12826861fc
Vallarian <danil.vorsin89@mail.ru>
2022-10-30 21:46:21 +0300

    New

---
+++ MyCoreWebApp/MyCoreWebApp/ViewModels/ChangeRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCoreWebApp.ViewModels
+{
+    public class ChangeRoleViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
